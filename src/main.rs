c591a0e1788bbdaddf50de0125880e9089fe4063
xurban5 <xurban5@fi.muni.cz>
2024-01-28 09:25:39 +0100

    fix: windows file printing

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use ui::app::App;
 
 use clap::Parser;
 
+use dunce;
+
 use crate::backend::disko_tree::DiskoTree;
 
 const DEFAULT_TICK_RATE: f64 = 4.0;
@@ -30,7 +32,7 @@ struct Arguments {
 fn main() -> Result<()> {
     let mut arguments = Arguments::parse();
 
-    arguments.path = arguments.path.canonicalize()?;
+    arguments.path = dunce::canonicalize(arguments.path)?;
 
     if !arguments.path.is_dir() {
         bail!("{} is not a directory", arguments.path.display());
