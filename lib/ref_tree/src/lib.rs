300f1fe65ec292c3fa23cc0947a959578ea04e9f
xurban5 <xurban5@fi.muni.cz>
2024-01-28 23:45:26 +0100

    refactor: remove model folder from ref_tree

--- lib/ref_tree/src/lib.rs
+++ lib/ref_tree/src/lib.rs
@@ -1,19 +1,17 @@
 #![warn(clippy::pedantic)]
 
-mod model {
-    pub mod tree_mod {
-        mod pretty_print;
-        mod tests;
-        pub mod tree;
-    }
-    pub mod node_mod {
-        pub mod node;
-        pub mod node_to_root_iterator;
-        mod tests;
-    }
+pub mod tree_mod {
+    mod pretty_print;
+    mod tests;
+    pub mod tree;
+}
+pub mod node_mod {
+    pub mod node;
+    pub mod node_to_root_iterator;
+    mod tests;
 }
 
 // Reexport Tree & Node for convenience.
-pub use model::node_mod::node::Node;
-pub use model::node_mod::node_to_root_iterator::NodeToRootIterator;
-pub use model::tree_mod::tree::Tree;
+pub use node_mod::node::Node;
+pub use node_mod::node_to_root_iterator::NodeToRootIterator;
+pub use tree_mod::tree::Tree;
